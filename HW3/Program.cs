a225a13032a684596450e971dbb8645f01f7fa10
Rodion23 <rodion23@mail.ru>
2023-03-03 22:04:24 +0700

    Add HW3

--- HW3/Program.cs
+++ HW3/Program.cs
@@ -2,3 +2,12 @@
 m = 2, n = 3 -> A(m,n) = 9
 m = 3, n = 2 -> A(m,n) = 29 */
 
+int AckermannFunction (int numberM, int numberN)
+{
+    if (numberM == 0) return numberN + 1;
+    if (numberM != 0 && numberN == 0) return AckermannFunction(numberM - 1, 1);
+    if (numberM > 0 && numberN > 0) return AckermannFunction(numberM - 1, AckermannFunction(numberM, numberN - 1));
+return AckermannFunction(numberM, numberN);
+}
+
+Console.WriteLine($"Функция Аккермана = {AckermannFunction(2, 3)}");
