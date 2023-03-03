47739331ac54a4a16f47de9b256b05c860d95c94
Rodion23 <rodion23@mail.ru>
2023-03-03 21:57:43 +0700

    Add HW2

--- HW2/Program.cs
+++ HW2/Program.cs
@@ -1,3 +1,15 @@
 ﻿/* Задача 2: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
 M = 1; N = 15 -> 120
 M = 4; N = 8. -> 30 */
+void NumberSum(int numberM, int numberN, int sum)
+{
+    if (numberM > numberN)
+    {
+        Console.WriteLine($"Сумма натуральных элементов в промежутке от M до N: {sum}");
+        return;
+    }
+    sum = sum + (numberM++);
+    NumberSum(numberM, numberN, sum);
+}
+
+NumberSum(1, 15, 0);
