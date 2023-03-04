aa2b30fe9fe5856bc030b255585215f9d80b85e8
Rodion23 <rodion23@mail.ru>
2023-03-04 13:49:57 +0700

    Add HW1

--- HW1/Program.cs
+++ HW1/Program.cs
@@ -1,3 +1,15 @@
 ﻿/* Задача 1: Задайте значения M и N. Напишите программу, которая выведет все чётные натуральные числа в промежутке от M до N с помощью рекурсии.
 M = 1; N = 5 -> "2, 4"
 M = 4; N = 8 -> "4, 6, 8" */
+void ShowNumbers(int start, int finish)
+{
+    if (start > finish)
+    {
+        return;
+    }
+    if (start % 2 == 0)
+        System.Console.Write($"{start} ");
+    ShowNumbers(start + 1, finish);
+}
+
+ShowNumbers(1, 10);
